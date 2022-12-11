5400c056e2510bd58cc33e59b2fd56abae2a9eec
MihaiMuntean <muntean.mihai70@gmail.com>
2022-12-11 11:40:04 +0200

    .net Core 3.1 app skeleton created

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using new_world_31.Models;
+using Newtonsoft.Json;
+
+namespace new_world_31.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public string Contact()
+        {
+            var user = new User {
+                Username = "Mihai",
+                Fullname = "Muntean Mihai",
+                Password = "parola"
+            };
+
+            var json = JsonConvert.SerializeObject(user, Formatting.Indented);
+
+            return json;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace new_world_31.Models
+{
+    public class User 
+    {
+        public string Username { get; set; }
+        public string Fullname { get; set; }
+        public string Password { get; set; }
+    }
+}
